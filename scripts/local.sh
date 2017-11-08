08566476bd8c293ab3808e663e0f0a1b53bfd697
Peter Stace <peter@dgraph.io>
2017-11-08 11:37:47 +1100

    Use hugo 0.19 for now

--- scripts/local.sh
+++ scripts/local.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-hugo server -w --baseURL=http://localhost:1313
+# The tour only works with hugo 0.19 for now. Get the binary from
+# https://github.com/gohugoio/hugo/releases/tag/v0.19
+hugo_0.19_linux_amd64 server -w --baseURL=http://localhost:1313
