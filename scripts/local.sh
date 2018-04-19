e2d2f4d081ff7236fc603f05a191280cce5f8598
Pawan Rawal <pawan@dgraph.io>
2018-04-19 13:38:08 +1000

    Update sidebar.html to work with Hugo v0.37 (#34)
    * Update sidebar.html to work with Hugo v0.37
    
    * Update

--- scripts/local.sh
+++ scripts/local.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-# The tour only works with hugo 0.19 for now. Get the binary from
-# https://github.com/gohugoio/hugo/releases/tag/v0.19
-hugo_0.19_linux_amd64 server -w --baseURL=http://localhost:1313
+hugo server -w --baseURL=http://localhost:1313
