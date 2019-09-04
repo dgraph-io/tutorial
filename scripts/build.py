f9d2bdfdaa9dc4ae20c8752ff270d1ddff0b9290
Paul Korzhyk <paul@dgraph.io>
2019-09-04 19:38:01 +0300

    Make latest release latest, now not later

--- scripts/build.py
+++ scripts/build.py
@@ -63,10 +63,7 @@ def buildBranch(branch, dest, jsonData):
     runHugo(dest)
 
 def buildAll(releases):
-    # TODO: once 1.1.0 is released remove the line below
-    #       and uncomment one after it
-    latestRelease = "1.0.16"
-    # latestRelease = releases[1]
+    latestRelease = releases[1]
     print('Latest Release (recommended to users): ', latestRelease)
 
     def jsonFor(version, latestRelease, releases):
