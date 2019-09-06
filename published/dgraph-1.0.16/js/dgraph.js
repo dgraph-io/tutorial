8c93d5208b99f61be30bd1ef7573c41145a2b84a
Paul Korzhyk <paul@dgraph.io>
2019-09-06 15:13:16 +0300

    Rebuild all with latest theme files

--- published/dgraph-1.0.16/js/dgraph.js
+++ published/dgraph-1.0.16/js/dgraph.js
@@ -1,77 +1,93 @@
-$(document).ready(function() {
-  $(document).on('click', 'a[data-action="toggle-expandable"]', function(e) {
-    e.preventDefault();
-    var $toggle = $(this);
-    var $expandable = $(this).closest('.expandable');
+import $ from "jquery";
 
-    $expandable.toggleClass('expanded');
-    $(body).toggleClass('noscroll');
+$('#tour-release').change(function(e) {
+  var dest = e.target.value;
+  if (dest == DgTour.thisRelease) {
+    return
+  }
+  if (dest == DgTour.latestRelease) {
+    window.location.href = "/"
+    return
+  }
+  if (dest != "master") {
+    dest = "dgraph-" + dest
+  }
+  window.location.href = "/" + dest
+});
 
-    var isExpanded = $expandable.hasClass('expanded');
-    if (isExpanded) {
-      $toggle.html("<i class='fa fa-caret-up'></i> Hide answer");
-    } else {
-      $toggle.html("<i class='fa fa-caret-down'></i> Show answer");
-    }
-  });
 
-  $(document).on('click', 'a[data-action="toggle-sidebar"]', function (e) {
-    e.preventDefault();
-    e.stopPropagation();
+$(document).on('click', 'a[data-action="toggle-expandable"]', function(e) {
+  e.preventDefault();
+  var $toggle = $(this);
+  var $expandable = $(this).closest('.expandable');
 
-    var $sidebar = $('#sidebar');
-    $sidebar.toggleClass('hidden');
+  $expandable.toggleClass('expanded');
 
-    var isShown = !$sidebar.hasClass('hidden');
-    if (isShown) {
-      $(".topic.active")[0].scrollIntoView();
-    }
-  });
+  var isExpanded = $expandable.hasClass('expanded');
+  if (isExpanded) {
+    $toggle.html("<i class='fa fa-caret-up'></i> Hide answer");
+  } else {
+    $toggle.html("<i class='fa fa-caret-down'></i> Show answer");
+  }
+});
 
-  $(document).on('click', '.categories .category', function (e) {
-    var $topicList = $(this).siblings('.topics');
-    $topicList.toggleClass('open');
-  });
+$(document).on('click', 'a[data-action="toggle-sidebar"]', function (e) {
+  e.preventDefault();
+  e.stopPropagation();
 
-  $(document).on('click', 'body', function (e) {
-    // Collapse sidebar if open
-    var $sidebar = $('#sidebar');
-    if (!$sidebar.hasClass('hidden')) {
-      $('#sidebar').addClass('hidden');
-    }
-  });
+  var $sidebar = $('#sidebar');
+  $sidebar.toggleClass('hidden');
 
-  $(document).on('click', '#sidebar', function (e) {
-    e.stopPropagation();
-  });
+  var isShown = !$sidebar.hasClass('hidden');
+  if (isShown) {
+    $(".topic.active")[0].scrollIntoView();
+  }
+});
 
-  var KEY_RIGHT = 39;
-  var KEY_LEFT = 37;
-  document.addEventListener('keydown', function (e) {
-    var isCodeMirrorFocused = $('.CodeMirror-focused').length > 0;
-    if (isCodeMirrorFocused) {
-      return;
-    }
+$(document).on('click', '.categories .category', function (e) {
+  var $topicList = $(this).siblings('.topics');
+  $topicList.toggleClass('open');
+  e.stopPropagation();
+});
 
-    var keyCode = e.keyCode;
-    if (keyCode === KEY_RIGHT) {
-      var nextHref = $("[data-action='go-next']").attr('href');
-      window.location.href = nextHref;
-    } else if (keyCode === KEY_LEFT) {
-      var prefHref = $("[data-action='go-prev']").attr('href');
-      window.location.href = prefHref;
-    }
-  });
+$(document).on('click', 'body', function (e) {
+  // Collapse sidebar if open
+  $('#sidebar').addClass('hidden');
+});
 
-  /************ On page load **/
-
-  // Add target = _blank to all external links.
-  var links = document.links;
-  for (var i = 0, linksLength = links.length; i < linksLength; i++) {
-    if (links[i].hostname != window.location.hostname) {
-      links[i].target = '_blank';
-    }
+var KEY_RIGHT = 39;
+var KEY_LEFT = 37;
+document.addEventListener('keydown', function (e) {
+  var isCodeMirrorFocused = $('.CodeMirror-focused').length > 0;
+  if (isCodeMirrorFocused) {
+    return;
   }
 
-  $('.topic.active').closest('.topics').addClass('open');
+  if (document.activeElement instanceof HTMLInputElement) {
+    // An input field is focused. Do not intercept arrow keys.
+    return;
+  }
+
+  var nextHref = null;
+  switch (e.keyCode) {
+    case 39:
+      // KEY_RIGHT
+      nextHref = $("[data-action='go-next']").attr('href');
+      break;
+    case 37:
+      // KEY_LEFT
+      nextHref = $("[data-action='go-prev']").attr('href');
+      break;
+  }
+  nextHref && (window.location.href = nextHref);
 });
+
+// Add target = _blank to all external links.
+var links = document.links;
+for (var i = 0, linksLength = links.length; i < linksLength; i++) {
+  if (links[i].hostname != window.location.hostname) {
+    links[i].target = '_blank';
+  }
+}
+
+$('.topic.active').closest('.topics').addClass('open');
