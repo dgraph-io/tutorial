078c368b45feac3e50467a9fe3b0940b427fefee
Prashant Shahi <prashant@dgraph.io>
2020-01-06 21:52:46 +0530

    Adding netlify and updating shell script

--- scripts/local.sh
+++ scripts/local.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-hugo server -w --baseURL=http://localhost:1313 --config config.toml,releases.json
+set -e
+
+GREEN='\033[32;1m'
+RESET='\033[0m'
+
+if [[ $1 == "-p" || $1 == "--preview" ]]; then
+    echo -e "$(date) $GREEN  Generating tutorial contents in the public folder. $RESET"
+    hugo \
+      --destination=public \
+      --baseURL="$DEPLOY_PRIME_URL" \
+      --config config.toml,releases.json 1> /dev/null
+    echo -e "$(date) $GREEN  Done creating local build in the public folder. $RESET"
+else
+    hugo server -w --baseURL=http://localhost:1313 --config config.toml,releases.json
+fi
