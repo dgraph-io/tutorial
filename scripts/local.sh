54e788b90837879330aec2e0be1a3cf046fc2047
Prashant Shahi <prashant@dgraph.io>
2020-01-06 22:52:29 +0530

    minor

--- scripts/local.sh
+++ scripts/local.sh
@@ -6,12 +6,12 @@ GREEN='\033[32;1m'
 RESET='\033[0m'
 
 if [[ $1 == "-p" || $1 == "--preview" ]]; then
-    echo -e "$(date) $GREEN  Generating tutorial contents in the public folder. $RESET"
+    echo -e "$(date) $GREEN  Generating tutorial static pages in the public folder. $RESET"
     hugo \
       --destination=public \
       --baseURL="$DEPLOY_PRIME_URL" \
       --config config.toml,releases.json 1> /dev/null
-    echo -e "$(date) $GREEN  Done creating local build in the public folder. $RESET"
+    echo -e "$(date) $GREEN  Done building. $RESET"
 else
     hugo server -w --baseURL=http://localhost:1313 --config config.toml,releases.json
 fi
