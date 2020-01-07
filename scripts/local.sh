042d71746c9689e49f85f7d623ab44927a2334db
Prashant Shahi <prashant@dgraph.io>
2020-01-07 18:09:24 +0530

    Adding PR preview feature and updating local.sh script (#115)
    * Adding netlify and updating shell script
    
    * removing netlify.toml file - switching it with surge.sh
    
    * removing baseurl - considers the domain itself

--- scripts/local.sh
+++ scripts/local.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-hugo server -w --baseURL=http://localhost:1313 --config config.toml,releases.json
+set -e
+
+GREEN='\033[32;1m'
+RESET='\033[0m'
+
+if [[ $1 == "-p" || $1 == "--preview" ]]; then
+    echo -e "$(date) $GREEN  Generating tutorial static pages in the public folder. $RESET"
+    hugo \
+      --destination=public \
+      --config config.toml,releases.json 1> /dev/null
+    echo -e "$(date) $GREEN  Done building. $RESET"
+else
+    hugo server -w --baseURL=http://localhost:1313 --config config.toml,releases.json
+fi
