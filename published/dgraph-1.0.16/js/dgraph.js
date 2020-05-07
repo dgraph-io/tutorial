f4cd08f03e0470e92ee7a40d701632d1194166e3
Daniel Mai <daniel@dgraph.io>
2020-05-07 08:32:42 -0700

    Hugo rebuild all branches

--- published/dgraph-1.0.16/js/dgraph.js
+++ published/dgraph-1.0.16/js/dgraph.js
@@ -6,13 +6,13 @@ $('#tour-release').change(function(e) {
     return
   }
   if (dest == DgTour.latestRelease) {
-    window.location.href = DgTour.home;
+    window.location.href = "/"
     return
   }
   if (dest != "master") {
     dest = "dgraph-" + dest
   }
-  window.location.href = DgTour.home + dest;
+  window.location.href = "/" + dest
 });
 
 
