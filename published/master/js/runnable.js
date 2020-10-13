9d07d11cfa7c4f56c3673787a30bea4bb99fa1ee
Debabrata <debabratab@geekyants.com>
2020-10-13 14:36:51 +0530

    Hugo rebuild all branches

--- published/master/js/runnable.js
+++ published/master/js/runnable.js
@@ -201,8 +201,12 @@ $(document).on('click', '.runnable [data-action="run"]', async function(e) {
     $currentRunnable.find('.output-container').addClass('error');
     // Ideally we should check that xhr.status === 404, but because we are doing
     // CORS, status is always 0
-    var defaultError = 'Error: Is Dgraph running locally?';
-    let message = error || defaultError;
+    var defaultError = 'Error: Is Dgraph running locally? or reachable?';
+    let message;
+
+    let res = await error.toString().match(/APIError/g);
+
+    !res ? message = defaultError : message = error
 
     codeEl.text(message);
   }
